2528f6045379c668768ec4e72546a7a09218c729
Danny Sun <danny.sun@companiondxlab.com>
2016-07-26 14:12:13 -0500

    add order key

--- db/migrate/20160710204735_remove_from_order.rb
+++ db/migrate/20160710204735_remove_from_order.rb
@@ -2,5 +2,6 @@ class RemoveFromOrder < ActiveRecord::Migration
   def change
     remove_column :orders, :sku
     remove_column :orders, :product_name
+    add_column :orders, :order_key
   end
 end
